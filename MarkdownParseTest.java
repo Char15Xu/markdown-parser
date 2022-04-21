e63b18904cb2c56ea2435371a062ff633db78fa7
Char15Xu <81684416+Char15Xu@users.noreply.github.com>
2022-04-21 11:26:36 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,5 +1,11 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.ArrayList;
+
 public class MarkdownParseTest {
 
     @Test
@@ -7,4 +13,11 @@ public class MarkdownParseTest {
         assertEquals(2, 1 + 1);
     }
 
+    @Test
+    public void MarkdownParse() throws IOException{
+        Path fileName = Path.of("test-file.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        assertEquals(List.of("https://something.com","some-thing.html"), links);
+    }
 }
