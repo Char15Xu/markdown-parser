6fba3b24dc1a0bf93a135cea724b313d76f0eb0f
Char15Xu <81684416+Char15Xu@users.noreply.github.com>
2022-04-23 23:32:41 -0700

    Update MarkdownParse.java
    fix for failtest2, simply check the positions of close bracket and open parenthesis before running the while loop

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -14,16 +14,18 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         int count = 0;
+        if (markdown.indexOf(")") > markdown.indexOf("[")){
+            while(currentIndex < markdown.length() && count < markdown.length()) {
+                int openBracket = markdown.indexOf("[", currentIndex);
+                int closeBracket = markdown.indexOf("]", openBracket);
+                int openParen = markdown.indexOf("(", closeBracket);
+                int closeParen = markdown.indexOf(")", openParen);
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+                currentIndex = closeParen + 1;
+                count = count + currentIndex;
+                }
+        }
         
-        while(currentIndex < markdown.length() && count < markdown.length()) {
-            int openBracket = markdown.indexOf("[", currentIndex);
-            int closeBracket = markdown.indexOf("]", openBracket);
-            int openParen = markdown.indexOf("(", closeBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
-            count = count + currentIndex;
-            }
         
         return toReturn;
     }
