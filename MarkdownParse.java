ffb9773addb75ed71d87304e4e66a07ba2bcffed
aHewig <ahewig@ucsd.edu>
2022-04-24 19:45:07 -0700

    Final MarkdownParse

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -9,19 +9,44 @@ public class MarkdownParse {
 
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
+        ArrayList<Integer> record = new ArrayList<Integer>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         if (markdown.contains("[") == true && markdown.contains("]") == true && markdown.contains("(") == true && markdown.contains(")") == true) {
             while(currentIndex < markdown.length()) {
+                record.add(currentIndex);
                 int openBracket = markdown.indexOf("[", currentIndex);
                 int closeBracket = markdown.indexOf("]", openBracket);
                 int openParen = markdown.indexOf("(", closeBracket);
                 int closeParen = markdown.indexOf(")", openParen);
-                toReturn.add(markdown.substring(openParen + 1, closeParen));
-                currentIndex = closeParen + 1;  
+                
+                currentIndex = closeParen + 1; 
+                
+                if(toReturn.contains(markdown.substring(openParen + 1, closeParen))){
+                    if(record.contains(currentIndex)){
+                        break;
+                    } else {
+                        toReturn.add(markdown.substring(openParen + 1, closeParen));
+                    }
+                } else {
+                    toReturn.add(markdown.substring(openParen + 1, closeParen));
+                } 
             }
         }
-        // If the link does not have brackets
+        // If the link only contains brackets
+        else if (markdown.contains("[") == true && markdown.contains("]") == true) {
+            while(currentIndex < markdown.length()) {
+                //int lastParen = markdown.lastIndexOf(")");
+                int openBracket = markdown.indexOf("[", currentIndex);
+                int closeBracket = markdown.indexOf("]", openBracket);
+                
+                int nextBracket = markdown.indexOf("[", closeBracket);
+
+                currentIndex = nextBracket;
+            }
+
+        }
+        // If the link only contains parentheses
         else if (markdown.contains("(") == true && markdown.contains(")") == true) {
             while(currentIndex < markdown.length()) {
                 int firstParen = markdown.indexOf("(", currentIndex);
--- test2.md
+++ test2.md
@@ -1,3 +1,6 @@
 # Lit Test File
 
-[link1](https://[dummm)y_link)
+[](link1)
+[](link 2()
+[](link 3) **
+[](link 4)))))))
