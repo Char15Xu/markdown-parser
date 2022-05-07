1314105cad0d766b38a38b063fa1133c74a89e15
Char15Xu <81684416+Char15Xu@users.noreply.github.com>
2022-05-07 15:41:23 -0700

    initial commit with java files and junit files.

---
+++ MarkdownParseTest.java
@@ -0,0 +1,36 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class MarkdownParseTest {
+
+    @Test
+    public void addition() {
+        assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void MarkdownParseTest1() throws IOException{
+        assertEquals(List.of("https://something.com","some-page.html"), 
+        MarkdownParse.getLinks(Files.readString(Path.of("failtest1.md"))));
+    }
+
+    @Test
+    public void MarkdownParseTest2() throws IOException{
+        assertEquals(new ArrayList<String>(), 
+        MarkdownParse.getLinks(Files.readString(Path.of("failtest2.md"))));
+    }
+
+
+    @Test
+    public void MarkdownParseTest3() throws IOException{
+        assertEquals(new ArrayList<String>(), 
+        MarkdownParse.getLinks(Files.readString(Path.of("failtest3.md"))));
+    }
+}
