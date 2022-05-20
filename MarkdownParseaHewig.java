1e464fe84d46f3567902e7720135c59beae4c0f3
Char15Xu <81684416+Char15Xu@users.noreply.github.com>
2022-05-19 23:29:41 -0700

    Add java file that I reviewed last time

---
+++ MarkdownParseaHewig.java
@@ -0,0 +1,84 @@
+//https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
+
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class MarkdownParseaHewig {
+
+    public static ArrayList<String> getLinks(String markdown) {
+        ArrayList<String> toReturn = new ArrayList<>();
+        ArrayList<Integer> record = new ArrayList<Integer>();
+        // find the next [, then find the ], then find the (, then read link upto next )
+        int currentIndex = 0;
+        if (markdown.contains("[") == true && markdown.contains("]") == true && markdown.contains("(") == true && markdown.contains(")") == true) {
+            while(currentIndex < markdown.length()) {
+                record.add(currentIndex);
+        int count = 0;
+        if (markdown.indexOf(")") > markdown.indexOf("[") & 
+            markdown.indexOf("!") + 1 != markdown.indexOf("[")){
+            while(currentIndex < markdown.length() && count < markdown.length()) {
+                int openBracket = markdown.indexOf("[", currentIndex);
+                int closeBracket = markdown.indexOf("]", openBracket);
+                int openParen = markdown.indexOf("(", closeBracket);
+                int closeParen = markdown.indexOf(")", openParen);
+                
+                currentIndex = closeParen + 1; 
+
+                if(toReturn.contains(markdown.substring(openParen + 1, closeParen))){
+                    if(record.contains(currentIndex)){
+                        break;
+                    } else {
+                        toReturn.add(markdown.substring(openParen + 1, closeParen));
+                    }
+                } else {
+                    toReturn.add(markdown.substring(openParen + 1, closeParen));
+                } 
+            }
+        }
+        // If the link only contains brackets
+        else if (markdown.contains("[") == true && markdown.contains("]") == true) {
+            while(currentIndex < markdown.length()) {
+                //int lastParen = markdown.lastIndexOf(")");
+                int openBracket = markdown.indexOf("[", currentIndex);
+                int closeBracket = markdown.indexOf("]", openBracket);
+                
+                int nextBracket = markdown.indexOf("[", closeBracket);
+
+                currentIndex = nextBracket;
+            }
+
+        }
+        // If the link only contains parentheses
+        else if (markdown.contains("(") == true && markdown.contains(")") == true) {
+            while(currentIndex < markdown.length()) {
+                int firstParen = markdown.indexOf("(", currentIndex);
+                int secondParen = markdown.indexOf(")", firstParen);
+                toReturn.add(markdown.substring(firstParen + 1, secondParen));
+                currentIndex = secondParen + 1;
+            }
+        } 
+        // To account for files with no links in them
+        else {
+            toReturn.add("A link was not found in this file.");
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+                currentIndex = closeParen + 1;
+                count = count + currentIndex;
+                }
+        }
+        
+        
+        return toReturn;
+    }
+ 
+
+    public static void main(String[] args) throws IOException {
+        Path fileName = Path.of(args[0]);
+        String content = Files.readString(fileName);
+        ArrayList<String> links = getLinks(content);
+        System.out.println(links);
+    }
+}
